c6be167313727a0455a1300e027adf480f254bef
Tyler <tyler@danstrom.com>
2018-11-24 23:11:23 -0600

    fixed bad string concat in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="Secret Santa Generator",
       author="Tyler Danstrom",
       author_email="tyler@danstrom.com",
       version="1.0.0",
-      description="A simple utility to take a list of participants and " +\ 
+      description="A simple utility to take a list of participants and " +
                   "assign a Secret Santa  to each participant",
       packages=['secretsanta'],
 )
