a76df0ba3d7b39ee25052fbbdf1ec76a380c579f
Tyler <tyler@danstrom.com>
2018-11-24 23:06:21 -0600

    changed version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(name="Secret Santa Generator",
       author="Tyler Danstrom",
       author_email="tyler@danstrom.com",
-      version="0.01",
-      description="A simple secret santa utility",
+      version="1.0.0",
+      description="A simple utility to take a list of participants and " +\ 
+                  "assign a Secret Santa  to each participant",
       packages=['secretsanta'],
 )
